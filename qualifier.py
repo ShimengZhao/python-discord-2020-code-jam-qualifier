7cbb409f1f77a7eba30c9b99b67b9f1410981287
Shimeng <zhaoshimeng@gmail.com>
2020-07-19 13:53:52 +0200

    basic requirements passed

--- qualifier.py
+++ qualifier.py
@@ -15,6 +15,8 @@ Important notes for submission:
 """
 import datetime
 import typing
+import re
+from collections import Counter
 
 
 class ArticleField:
@@ -28,4 +30,32 @@ class Article:
     """The `Article` class you need to write for the qualifier."""
 
     def __init__(self, title: str, author: str, publication_date: datetime.datetime, content: str):
-        pass
+        self.title = title
+        self.author = author
+        self.publication_date = publication_date
+        self.content = content
+
+    def __repr__(self):
+        return f'<Article title="{self.title}" author=\'{self.author}\' publication_date=\'{self.publication_date.isoformat()}\'>'
+
+    def __len__(self):
+        return len(self.content)
+
+    def short_introduction(self, n_characters: int) -> str:
+        raw_characters = self.content[:n_characters + 1]
+        if n_characters == 0:
+            return ""
+        else:
+            if raw_characters[-1] == " " or raw_characters[-1] == "\n":
+                return raw_characters[:-1]
+            else:
+                while len(raw_characters) > 0:
+                    if raw_characters[-1] == " " or raw_characters[-1] == '\n':
+                        return raw_characters[:-1]
+                    raw_characters = raw_characters[:-1]
+                return ""
+
+    def most_common_words(self, n_words: int) -> dict:
+        word_list = re.findall(r'\w+', self.content.lower())
+        result = Counter(word_list).most_common(n_words)
+        return dict(result)
